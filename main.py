6c60a16d93d5d69622ee19320bc46c0e36222165
riyadsarsour <riyadsarsour@live.com>
2020-12-17 02:25:32 -0500

    slight tweaks

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
         msg = input()
 
     number = input(
-        "Type the Phone number you would like to text, number should be fomratted like '1234567890'\n")
+        "Type the Phone number you would like to text, number should be formatted like '1234567890'\n")
     while len(number) < 10 or len(number) > 10:
         number = input(
             "Invalid lengthed number\n Enter 10 digit number below\n")
     for word in words:
         send_message(number, word)
     send_message(number, "-Sent from Riyad's Bot :D")
-
+    print("\n")
     print("Thank you for using Dexter the Texter\n")
